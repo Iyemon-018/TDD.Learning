7d27deacfda21709a4ba4343b004af855fe5cbe4
石井 健太 <i_m_a_walker@live.jp>
2022-03-21 21:16:52 +0900

    前回のコミットにコメントを付与した。

--- 02.ボウリング/BowlingGame/Program.cs
+++ 02.ボウリング/BowlingGame/Program.cs
@@ -16,8 +16,15 @@ namespace BowlingGame
     {
         // cf. https://www.slideshare.net/mdclement/bowling-game-kata-in-c-adapted/41
         // ここまで完了した。
+
+        /// <summary>
+        /// 投球ごとに倒したピンの数を記録します。
+        /// </summary>
         private readonly int[] _rolls = new int[21];
 
+        /// <summary>
+        /// 現在の投球回数です。
+        /// </summary>
         private int _currentRoll;
 
         public void Roll(int pins)
@@ -27,17 +34,19 @@ namespace BowlingGame
 
         public int Score()
         {
-            var score = 0;
-            var roll  = 0;
+            var score = 0;          // 最終的なスコアの計算結果はここに格納されます。
+            var roll  = 0;          // 投球回数の参照用インデックスです。
 
             for (int frame = 0; frame < 10; frame++)
             {
                 if (IsSpare(roll))
                 {
+                    // スペア発生時の得点数を計算する。
                     score += 10 + _rolls[roll + 2];
                 }
                 else
                 {
+                    // いずれの条件にも一致しなかったので、ボーナス無しの得点数を計算する。
                     score += _rolls[roll] + _rolls[roll + 1];
                 }
                 roll += 2;
